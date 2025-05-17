f94b4434e0e6a5b5d60a6a4292a012b304cd121c
Ajay Singh <ajaybkn1108@gmail.com>
2025-05-17 15:43:51 +0530

    login implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,11 +4,17 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!password || !fullName || !email) {
+      return res.status(400).send({
+        message: "all fields are required",
+      });
+    }
     if (password && password.length < 6) {
       return res.status(400).send({
         message: "Password must be atleast 6 characters",
       });
     }
+
     const user = await User.findOne({ email });
     if (user) return res.status(400).send({ message: "Email already exists" });
     const salt = await bcrypt.genSalt(10);
@@ -38,8 +44,33 @@ export const signup = async (req, res) => {
     });
   }
 };
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).send({
+        message: "All fields are required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        message: "user is not registered",
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(404).send({
+        message: "Invalid Credentials",
+      });
+    }
+    generateToken(user._id, res);
+    user.password = "";
+    res.status(200).send({
+      message: "login successfull",
+      user,
+    });
+  } catch (error) {}
 };
 export const logout = (req, res) => {
   res.send("logout route");
