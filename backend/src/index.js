3a600c1e73528fc0cd86a387adb06b20dda8d66f
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-06-22 18:22:15 +0530

    socket io implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,20 +5,21 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
-const app = express();
+import { app, server } from "./lib/socket.js";
+app;
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
-  })
+	cors({
+		origin: ["http://localhost:5173", "http://localhost:5174"],
+		credentials: true,
+	})
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => {
-  console.log(`server is running on port-${PORT}`);
-  connectDB();
+server.listen(PORT, () => {
+	console.log(`server is running on port-${PORT}`);
+	connectDB();
 });
